064ba5fd1fc45612bc8226ad0e04ff0e8bfb0745
Mauro Altamura <altamura.mauro@gmail.com>
2024-06-09 17:31:42 +0200

    Ottimizzazione Countdown

--- src/js/esercizio_4.js
+++ src/js/esercizio_4.js
@@ -7,30 +7,40 @@ Al termine del countdown richiama la funzione sveglia()
 let numeroIniziale = 10;
 const timerTag = document.getElementById("timer");
 const buttonTag = document.querySelector("button");
+let timerId = null; // Aggiungi una variabile per controllare il timer
 
 timerTag.innerHTML = numeroIniziale;
 
 buttonTag.addEventListener("click", countdown);
 
 function countdown() {
-	function count() {
-		timerTag.innerHTML = numeroIniziale;
-		numeroIniziale--;
-		if (numeroIniziale < 0) {
-			clearInterval(timerId);
+    // Disabilita il pulsante per evitare ulteriori click
+    buttonTag.disabled = true;
+		buttonTag.classList.add("disabled");
+
+    function count() {
+        timerTag.innerHTML = numeroIniziale;
+        numeroIniziale--;
+        if (numeroIniziale < 0) {
+            clearInterval(timerId);
+            timerId = null; // Resetta il timerId
             sveglia();
-		}
-	}
-	const timerId = setInterval(count, 1000);
+        }
+    }
+
+    // Avvia il timer solo se non è già in esecuzione
+    if (!timerId) {
+        timerId = setInterval(count, 1000);
+    }
 }
 
-/*non c'è bisogno di modificare la funzione sveglia()*/
+/* non c'è bisogno di modificare la funzione sveglia() */
 function sveglia() {
-	const audio = document.querySelector("audio");
-	audio.currentTime = 0.2;
-	audio.play();
-	document.getElementById("sveglia").style.display = "block";
-	setTimeout(function () {
-		location.reload();
-	}, 4800);
+    const audio = document.querySelector("audio");
+    audio.currentTime = 0.2;
+    audio.play();
+    document.getElementById("sveglia").style.display = "block";
+    setTimeout(function () {
+        location.reload();
+    }, 4800);
 }
